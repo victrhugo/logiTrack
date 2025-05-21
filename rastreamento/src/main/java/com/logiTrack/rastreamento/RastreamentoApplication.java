800a3592103de48818c858cf81171a98e4dd4d99
Victor Hugo <victorhugonego@gmail.com>
2025-05-21 20:38:13 -0300

    api logiTrack

---
+++ rastreamento/src/main/java/com/logiTrack/rastreamento/RastreamentoApplication.java
@@ -0,0 +1,13 @@
+package com.logiTrack.rastreamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreamentoApplication.class, args);
+	}
+
+}
